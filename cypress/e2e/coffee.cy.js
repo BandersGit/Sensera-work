bc092b02ee52cc8d9429fceee34191cf5506875c
SiddhiQi <kim.siddiqi@gmail.com>
2023-10-02 13:01:40 +0200

    - added tests
    Co-authored-by: Andreas Nyström <BandersGit@users.noreply.github.com>

--- cypress/e2e/coffee.cy.js
+++ cypress/e2e/coffee.cy.js
@@ -44,17 +44,48 @@ describe('Coffee tests', () => {
 
   it('Should have a script element inside head element with "defer" attribute', () => {
     cy.get('head script[defer]').should('exist')
-    
+
   })
 
   //Todo: fyll eventuellt på med resterande tester av struktur och attribut
 
-  //Todo: test not working, not finished
-  it('Should display total cost of added items', () => {
-    cy.get('div.coffee-item').then($coffeeItems => {
-      $coffeeItems.forEach(item => {
-        //cy.find('add-button').click()
-      })
+
+
+
+  it('Should display the correct quantity of added coffee items', () => {
+    cy.get('.add-button').then(($addBtn) => {
+      cy.wrap($addBtn).click({ multiple: true }).click({ multiple: true })
+      .get('.quantity').filter(':contains("2")').should('have.length', $addBtn.length)
     })
   })
+
+  //Test not done
+  it('Should display the correct quantity of coffee items after subtract button is clicked', () => {
+    cy.get('.add-button').then(($addBtn) => {
+      cy.wrap($addBtn).click({ multiple: true }).click({ multiple: true })
+      .get('.quantity').filter(':contains("2")').should('have.length', $addBtn.length)
+    })
+  })
+
+  it('Should display total cost of added and reduced coffee items', () => {
+
+    let totalCost = 0
+    cy.get('.coffee-item').each(($item) => {
+
+      cy.wrap($item).find('.add-button').click().click()
+      cy.wrap($item).invoke('attr', 'data-price').then(price => {
+        totalCost += price * 2
+        console.log(typeof totalCost)
+      })
+      cy.wrap($item).find('.subtract-button').click()
+      cy.wrap($item).invoke('attr', 'data-price').then(price => {
+        totalCost -= price
+        console.log(typeof totalCost)
+      })
+    }).then(() => {
+      cy.get('#total-amount').invoke('text').then(parseInt).should('eq', totalCost)
+    })
+  })
+
+
 })
